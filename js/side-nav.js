9dad53d15a2f60d7ae88e3632afdff42309c7e8e
jiscocozza <jiscocozza@gmail.com>
2020-05-17 21:39:34 -0300

    Worked on "side-nav" and new "review article" page

---
+++ js/side-nav.js
@@ -0,0 +1,8 @@
+hamburguer.addEventListener("click", () => {
+    // console.log("Hiciste click!");
+    // nav.style.display = "block";
+
+// classList: add / remove / toggle
+
+sideNavContainer.classList.toggle("show-side-nav");
+});
